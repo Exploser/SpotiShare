32b95d7c481d0834bb0685d20c431a1d242d47c9
Exploser <exploserbusiness@gmail.com>
2024-05-16 19:02:06 -0400

    Remove dead code

--- src/_components/navbar.tsx
+++ src/_components/navbar.tsx
@@ -1,10 +1,11 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export function Navbar(){
 
     return (
       <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
         <div>Wrapify</div>
+
         <div className="flex flex-row gap-4 items-center">
             <SignedOut>
                 <SignInButton />
