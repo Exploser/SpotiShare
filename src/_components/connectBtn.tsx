db36e97bcb3ed1a24093cda4094253b72dde4448
Exploser <exploserbusiness@gmail.com>
2024-05-16 14:54:16 -0400

    Problem:1

---
+++ src/_components/connectBtn.tsx
@@ -0,0 +1,10 @@
+export function connectBtn() {
+    return (
+        <div className="flex w-full items-center justify-between p-4 text-xl font-semibold">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+
+            </button>    
+        </div>
+
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello User$$
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#16ed84] to-[#2aa80b] text-white">
     </main>
   );
 }
