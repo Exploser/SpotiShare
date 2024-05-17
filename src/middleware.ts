2cb4bbe10e7a4597f769ddf810a04becfcb4dff1
Exploser <exploserbusiness@gmail.com>
2024-05-17 00:19:03 -0400

    Update middleware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,13 @@
-// middleware.ts
-import {
-    clerkMiddleware,
-    createRouteMatcher
-  } from "@clerk/nextjs/server"
-  
-  const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
-  
-  export default clerkMiddleware((auth, request) => {
-    if (isProtectedRoute(request)) auth().protect()
-  })
-  
-  export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-  }
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    '/((?!.*\\..*|_next).*)', // Matches all paths except static files and _next folder
+    '/',                      // Matches the root path
+    '/api/(.*)',              // Matches all routes under /api
+    '/callback',              // Matches the /callback route
+    '/(trpc)(.*)',            // Matches all routes under /trpc
+  ],
+};
