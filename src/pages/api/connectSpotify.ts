a4e1692c97a81089f20355e417f2d71aed7ac922
Exploser <exploserbusiness@gmail.com>
2024-05-16 22:12:06 -0400

    Add server function to get spotify auth

---
+++ src/pages/api/connectSpotify.ts
@@ -0,0 +1,30 @@
+// src/pages/api/connectSpotify.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const client_id = process.env.SPOTIFY_CLIENT_ID || 'CLIENT_ID';
+const redirect_uri = process.env.SPOTIFY_REDIRECT_URI || 'http://localhost:3000/callback';
+
+const generateRandomString = (length: number) => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const state = generateRandomString(16);
+  const scope = 'playlist-read-private user-top-read user-read-recently-played user-top-read';
+
+  const params = new URLSearchParams({
+    response_type: 'code',
+    client_id: client_id,
+    scope: scope,
+    redirect_uri: redirect_uri,
+    state: state,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
+}
