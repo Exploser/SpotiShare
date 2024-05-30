96cfa4a5bd9f6a75ef913d2df42fef1529ddb88b
Exploser <exploserbusiness@gmail.com>
2024-05-30 19:56:33 -0400

    Fix saving on database

--- src/pages/api/saveTracks.ts
+++ src/pages/api/saveTracks.ts
@@ -1,108 +1,124 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getAuth } from '@clerk/nextjs/server';
-import { Client } from 'pg';
-import { tracks } from '~/server/db/schema';
-
-const client = new Client({
-  connectionString: process.env.POSTGRES_URL, // Ensure this is set in your environment variables
-});
-
-client.connect();
+import { albums, artists, savedTracks, tracks, users } from "~/server/db/schema";
+import { db } from '~/server/db';
+import { SpotifyTopTracksResponse } from '~/app/toptracks/page';
 
 const saveTracks = async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === 'POST') {
-      try {
-        const { userId } = getAuth(req);
-  
-        if (!userId) {
-          return res.status(401).json({ message: 'Unauthorized' });
-        }
-  
-        let tracksData = req.body;
+  if (req.method === 'POST') {
+    try {
+      const { userId } = getAuth(req);
 
-        if (!tracksData) {
-          return res.status(400).json({ message: 'No tracks data provided' });
+      if (!userId) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      } else {
+        try {
+          await db.insert(users).values({
+            id: userId,
+          });
+        } catch (e) {
+          console.error('Error inserting user:', e);
         }
-        
-  
-        // Ensure tracksData is an array
-        if (!Array.isArray(tracksData)) {
-          tracksData = [tracksData];
-        }
-        
-        if(tracksData.length>0){
-            const tracks = tracksData[0].tracks;
-        
-        // Start a transaction
-        await client.query('BEGIN');
-        for (const trackData of tracks) {
-            const album = trackData.album;
-            const artists = trackData.artists;
-            console.log([album.id, album.name, album.release_date, album.total_tracks])
-            
-            // Insert or update the album
-            const albumResult = await client.query(
-                `INSERT INTO albums (id, name, release_date, total_tracks)
-                VALUES ($1, $2, $3, $4)
-                ON CONFLICT (id) DO UPDATE SET name = EXCLUDED.name
-                RETURNING id`,
-                [album.id, album.name, album.release_date, album.total_tracks]
-            );
-            
-            console.log('Track : ' + trackData.name);
-          // Insert images related to the album
-          for (const img of album.images) {
-            await client.query(
-              `INSERT INTO images (url, height, width, album_id)
-               VALUES ($1, $2, $3, $4)
-               ON CONFLICT DO NOTHING`,
-              [img.url, img.height, img.width, albumResult.rows[0].id]
-            );
-          }
-  
-          // Insert or update artists
-          for (const artist of artists) {
-            await client.query(
-              `INSERT INTO artists (id, name, external_url)
-               VALUES ($1, $2, $3)
-               ON CONFLICT (id) DO UPDATE SET name = EXCLUDED.name`,
-              [artist.id, artist.name, artist.external_urls.spotify]
-            );
-          }
-  
-          // Insert the track
-          await client.query(
-            `INSERT INTO tracks (id, name, preview_url, track_number, external_url, album_id, user_id)
-             VALUES ($1, $2, $3, $4, $5, $6, $7)
-             ON CONFLICT (id) DO NOTHING`,
-            [trackData.id, trackData.name, trackData.preview_url, trackData.track_number, trackData.external_urls.spotify, album.id, userId]
-          );
-          
-  
-          // Insert relations between tracks and artists
-          for (const artist of artists) {
-            await client.query(
-              `INSERT INTO track_artists (track_id, artist_id)
-               VALUES ($1, $2)
-               ON CONFLICT DO NOTHING`,
-              [trackData.id, artist.id]
-            );
-          }
-        }
-  
-        // Commit the transaction
-        await client.query('COMMIT');
-    }
-  
-        res.status(200).json({ message: 'Tracks saved successfully' });
-      } catch (error: any) {
-        await client.query('ROLLBACK');
-        res.status(500).json({ message: 'Failed to save tracks', error: error.message });
       }
-    } else {
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+
+      const tracksData = req.body as SpotifyTopTracksResponse;
+
+      if (!tracksData) {
+        return res.status(400).json({ message: 'No tracks data provided' });
+      }
+
+      if (tracksData.items.length > 0) {
+        const trackList = tracksData.items;
+
+        for (const trackData of trackList) {
+          const trackAlbum = trackData.album;
+          const trackArtist = trackData.artists;
+
+          if (!trackAlbum) {
+            console.error('Album data or album id is missing:', trackAlbum);
+            continue; // Skip to the next trackData
+          }
+
+          try {
+            await db.insert(albums).values({
+              id: trackAlbum.id,
+              name: trackAlbum.name,
+              release_date: trackAlbum.release_date,
+              total_tracks: trackAlbum.total_tracks.toString(),
+            });
+          } catch (error) {
+            console.error('Error inserting album:', error);
+            continue; // Skip to the next trackData if album insertion fails
+          }
+
+          if (!trackArtist || !Array.isArray(trackArtist)) {
+            console.error('Artists data is missing or not an array:', artists);
+            continue; // Skip to the next trackData
+          }
+
+          for (const artist of trackArtist) {
+            if (!artist.id) {
+              console.error('Artist id is missing:', artist);
+              continue; // Skip to the next artist
+            }
+
+            try {
+              await db.insert(artists).values({
+                id: artist.id,
+                name: artist.name,
+              });
+            } catch (error) {
+              console.error('Error inserting artist:', error);
+            }
+          }
+
+          if (!trackData.id) {
+            console.error('Track id is missing:', trackData);
+            continue; // Skip to the next trackData
+          }
+
+          try {
+            await db.insert(tracks).values({
+              id: trackData.id,
+              name: trackData.name,
+              preview_url: trackData.preview_url,
+              track_number: trackData.track_number.toString(), // Convert track_number to string
+              album_name: trackAlbum.name,
+              artists_name: trackArtist.map((artist) => artist.name).join(', '),
+              user_id: userId,
+              popularity: trackData.popularity?.toString() || '0', // Convert popularity to string or default to '0'
+            });
+          } catch (error) {
+            console.error('Error inserting track:', error);
+          }
+
+          try{
+            await db.insert(savedTracks).values({
+              user_id: userId,
+              track_name: trackData.name,
+              album_name: trackAlbum.name,
+              artists_name: trackArtist.map((artist) => artist.name).join(', '),
+            });
+          } catch (error) {
+            console.error('Error inserting saved track:', error);
+          }
+        }
+
+        res.status(200).json({ message: 'Tracks saved successfully' });
+      } else {
+        res.status(400).json({ message: 'No tracks data available' });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json({ message: 'Failed to save tracks', error: error.message });
+      } else {
+        res.status(500).json({ message: 'Failed to save tracks' });
+      }
     }
-  };
-  
-  export default saveTracks;
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+};
+
+export default saveTracks;
