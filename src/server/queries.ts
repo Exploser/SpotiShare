e3da188d975237a4d97fa59c81518be46b5cafc0
Exploser <exploserbusiness@gmail.com>
2024-05-31 13:23:18 -0400

    Add database query to fetch saved content

---
+++ src/server/queries.ts
@@ -0,0 +1,17 @@
+import "server-only"
+import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
+import { savedTracks } from "./db/schema";
+
+export async function getMyTracks() {
+    const user = auth();
+  
+    if (!user.userId) throw new Error("Unauthorized");
+
+    const tracks = await db.query.savedTracks.findMany({
+      where: (model, { eq }) => eq(model.user_id, user.userId),
+      orderBy: (model, { desc }) => desc(model.id),
+    });
+    
+    return tracks;
+  }
