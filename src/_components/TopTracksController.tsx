8ac282556d21f3eb7866ae73df102ac730f0a9e0
Exploser <exploserbusiness@gmail.com>
2024-05-31 00:36:06 -0400

    Add dynamic error handling

--- src/_components/TopTracksController.tsx
+++ src/_components/TopTracksController.tsx
@@ -1,7 +1,6 @@
-// src/components/TopTracksController.tsx
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import VolumeController from './VolumeController';
 
 interface TopTracksControllerProps {
@@ -10,6 +9,7 @@ interface TopTracksControllerProps {
   limit: number;
   setLimit: (value: number) => void;
   handleRefetch: () => void;
+  items: any[];
 }
 
 const TopTracksController: React.FC<TopTracksControllerProps> = ({ timeRange, setTimeRange, limit, setLimit, handleRefetch }) => {
@@ -24,6 +24,9 @@ const TopTracksController: React.FC<TopTracksControllerProps> = ({ timeRange, se
   return (
     <div className="flex flex-col items-center text-white justify-center p-6 bg-gradient-to-bl from-green-700 via-gray-900 to-green-900 rounded-lg shadow-xl transition-transform transform my-12" id="top-tracks-controller">
       <h1 className="text-3xl font-bold text-white mb-4">Customize Track Options</h1>
+      {refetchAttempted && (timeRange === initialTimeRange || limit === initialLimit) && (
+        <p className="text-red-500 mt-2">Please change the time range or offset to refetch tracks.</p>
+      )}
       <div className="flex flex-row my-2">
         <div className="flex flex-col justify-between mr-4">
           <label className="mb-2">
@@ -53,8 +56,8 @@ const TopTracksController: React.FC<TopTracksControllerProps> = ({ timeRange, se
       </div>
       <VolumeController />
       <button
-        onClick={handleRefetch}
-        className="bg-blue-500 text-white px-4 py-2 rounded-md my-2"
+        onClick={handleRefetchIfDifferent}
+        className={`bg-blue-500 text-white px-4 py-2 rounded-md my-2`}
       >
         Refetch Tracks
       </button>
