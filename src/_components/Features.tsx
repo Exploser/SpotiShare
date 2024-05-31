30ff4d55b3b0b36abbed64b1e430d6f0b630fde0
Exploser <exploserbusiness@gmail.com>
2024-05-31 13:21:57 -0400

    Add saved content button on Features component

--- src/_components/Features.tsx
+++ src/_components/Features.tsx
@@ -3,16 +3,23 @@ const SpotifyFeatures: React.FC = () => {
         <div className="flex flex-col items-center bg-opacity-10 p-6 rounded-xl shadow-xl my-8  transition-transform transform hover:scale-105 "
             id="spotify-features">
             <h1 className="text-3xl font-bold text-white mb-4">Features!</h1>
-                <a className="bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 border border-green-700 rounded my-4 h-fit hover:scale-105"
+            <div id="spotify-features-buttons">
+                <a className="bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 border border-green-700 rounded whitespace-nowrap text-center my-4 h-fit hover:scale-105"
                     href="/toptracks"
                 >
                     Top Tracks
                 </a>
-                <a className="bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 border border-green-700 rounded my-4 h-fit hover:scale-105"
+                <a className="bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 border border-green-700 rounded whitespace-nowrap text-center my-4 h-fit hover:scale-105"
                     href="/topartists"
                 >
                     Top Artists
                 </a>
+            </div>
+            <a className="bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 border border-green-700 rounded my-4 h-fit hover:scale-105"
+                    href="/savedcontent"
+                >
+                    Saved Content
+            </a>
             <h1 className="text-xl font-bold text-white my-4">More features coming soon....</h1>
         </div>
     )
