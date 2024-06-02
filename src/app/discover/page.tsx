0740e2559ff2377c2b9df9bb3d88fb212dc7f589
Exploser <exploserbusiness@gmail.com>
2024-06-02 15:42:03 -0400

    Add interface for spotify's api response

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,6 +1,89 @@
+// Code: src/app/discover/page.tsx
+
 'use client';
+
 import { useEffect, useState } from "react";
-import { SpotifyTopTracksResponse } from "../toptracks/page";
+import SavedController from "~/_components/SavedController";
+
+interface Image {
+    url: string;
+    height: number;
+    width: number;
+  }
+  
+  interface ExternalUrls {
+    spotify: string;
+  }
+  
+  interface Artist {
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    name: string;
+    type: string;
+    uri: string;
+    followers?: {
+      href: string;
+      total: number;
+    };
+    genres?: string[];
+    images?: Image[];
+    popularity?: number;
+  }
+  
+  interface Album {
+    album_type: string;
+    total_tracks: number;
+    available_markets: string[];
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    images: Image[];
+    name: string;
+    release_date: string;
+    release_date_precision: string;
+    restrictions?: {
+      reason: string;
+    };
+    type: string;
+    uri: string;
+    artists: Artist[];
+  }
+  
+  interface ExternalIds {
+    isrc: string;
+    ean: string;
+    upc: string;
+  }
+  
+  interface Track {
+    album: Album;
+    artists: Artist[];
+    available_markets: string[];
+    disc_number: number;
+    duration_ms: number;
+    explicit: boolean;
+    external_ids: ExternalIds;
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    is_playable: boolean;
+    linked_from?: any; // Adjust the type if you have a specific structure for this field
+    restrictions?: {
+      reason: string;
+    };
+    name: string;
+    popularity: number;
+    preview_url: string;
+    track_number: number;
+    type: string;
+    uri: string;
+    is_local: boolean;
+  }
+  
+  interface TracksResponse {
+    tracks: Track[];
+  }
 
 export default function discover() {
     const [error, setError] = useState<string | null>(null);
