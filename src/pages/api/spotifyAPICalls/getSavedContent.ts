dcab87def12a949f2d009f7a2447714fcc7175be
Exploser <exploserbusiness@gmail.com>
2024-06-03 13:30:53 -0400

    Add server function to fetch saved content

---
+++ src/pages/api/spotifyAPICalls/getSavedContent.ts
@@ -0,0 +1,25 @@
+// src/pages/api/spotifyAPICalls/getTopArtists.ts
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getAuth } from '@clerk/nextjs/server';
+import { getMyTracks } from '~/server/queries';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const { userId } = getAuth(req);
+
+        if (!userId) { return res.status(401).json({ error: 'Unauthorized' }) };
+
+        const topArtists = await getMyTracks(userId);
+
+        res.status(200).json(topArtists);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+            res.status(500).json({ error: error.message });
+        } else {
+            console.error('Unexpected error', error);
+            res.status(500).json({ error: 'An unexpected error occurred' });
+        }
+    }
+}
