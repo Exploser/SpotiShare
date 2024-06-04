df7801806a4822ff2004e1676c9b18f97ca52e04
Exploser <exploserbusiness@gmail.com>
2024-06-04 18:07:38 -0400

    Add getGenres component

---
+++ src/pages/api/spotifyAPICalls/getGenres.ts
@@ -0,0 +1,29 @@
+// src/pages/api/spotifyAPICalls/getGenres.ts
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { parse } from 'cookie';
+import { fetchSpotifyGenres} from '../../../lib/spotify';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const cookies = req.headers.cookie;
+        if (!cookies) throw new Error('No cookies found');
+
+        const parsedCookies = parse(cookies);
+        const accessToken = parsedCookies.spotify_access_token;
+        if (!accessToken) throw new Error('Spotify access token not found');
+
+        
+        const getGenres = await fetchSpotifyGenres(accessToken);
+
+        res.status(200).json(getGenres);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+            res.status(500).json({ error: error.message });
+        } else {
+            console.error('Unexpected error', error);
+            res.status(500).json({ error: 'An unexpected error occurred' });
+        }
+    }
+}
