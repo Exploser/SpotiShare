b7c88dd98b01f21a77dd45f5317d62e30a9825f2
Exploser <exploserbusiness@gmail.com>
2024-06-04 16:28:09 -0400

    Update the interfaces

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -6,90 +6,92 @@ import { useEffect, useState } from "react";
 import SavedController from "~/_components/SavedController";
 
 interface Image {
-    url: string;
-    height: number;
-    width: number;
-  }
-  
-  interface ExternalUrls {
-    spotify: string;
-  }
-  
-  interface Artist {
-    external_urls: ExternalUrls;
+  url: string;
+  height: number;
+  width: number;
+}
+
+interface ExternalUrls {
+  spotify: string;
+}
+
+interface Artist {
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+  followers?: {
     href: string;
-    id: string;
-    name: string;
-    type: string;
-    uri: string;
-    followers?: {
-      href: string;
-      total: number;
-    };
-    genres?: string[];
-    images?: Image[];
-    popularity?: number;
-  }
-  
-  interface Album {
-    album_type: string;
-    total_tracks: number;
-    available_markets: string[];
-    external_urls: ExternalUrls;
-    href: string;
-    id: string;
-    images: Image[];
-    name: string;
-    release_date: string;
-    release_date_precision: string;
-    restrictions?: {
-      reason: string;
-    };
-    type: string;
-    uri: string;
-    artists: Artist[];
-  }
-  
-  interface ExternalIds {
-    isrc: string;
-    ean: string;
-    upc: string;
-  }
-  
-  interface Track {
-    album: Album;
-    artists: Artist[];
-    available_markets: string[];
-    disc_number: number;
-    duration_ms: number;
-    explicit: boolean;
-    external_ids: ExternalIds;
-    external_urls: ExternalUrls;
-    href: string;
-    id: string;
-    is_playable: boolean;
-    linked_from?: any; // Adjust the type if you have a specific structure for this field
-    restrictions?: {
-      reason: string;
-    };
-    name: string;
-    popularity: number;
-    preview_url: string;
-    track_number: number;
-    type: string;
-    uri: string;
-    is_local: boolean;
-  }
-  
-  interface TracksResponse {
-    tracks: Track[];
-  }
+    total: number;
+  };
+  genres?: string[];
+  images?: Image[];
+  popularity?: number;
+}
+
+interface Album {
+  album_type: string;
+  total_tracks: number;
+  available_markets: string[];
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  restrictions?: {
+    reason: string;
+  };
+  type: string;
+  uri: string;
+  artists: Artist[];
+}
+
+interface ExternalIds {
+  isrc: string;
+  ean: string;
+  upc: string;
+}
+
+interface Track {
+  album: Album;
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: ExternalIds;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  is_playable: boolean;
+  linked_from?: any; // Adjust the type if you have a specific structure for this field
+  restrictions?: {
+    reason: string;
+  };
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+  is_local: boolean;
+}
+
+interface TracksResponse {
+  tracks: Track[];
+}
 
 export default function Discover() {
-    const [tracks, setTracks] = useState<Track[]>([]);
-    const [error, setError] = useState<string | null>(null);
-
-    const [isMounted, setIsMounted] = useState(false);
+  const [tracks, setTracks] = useState<Track[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [seed_tracks, setSeedTracks] = useState('');
+  const [seed_artists, setSeedArtists] = useState('');
+  const [seed_genres, setSeedGenres] = useState('heavy-metal');
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     // Set isMounted to true when the component mounts
