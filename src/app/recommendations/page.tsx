1a4ab6e6eef04efa4ec9eb125503de9177d08244
Exploser <exploserbusiness@gmail.com>
2024-06-04 17:36:10 -0400

    Update styling for recommendations

--- src/app/recommendations/page.tsx
+++ src/app/recommendations/page.tsx
@@ -138,62 +138,82 @@ export default function Recommendations() {
   };
 
   return (
-    <div className="flex flex-col flex-center items-center">
-      <h1>Choose Tracks</h1>
-      <div className="max-w-screen-lg h-full w-fit text-white">
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-2 w-full">
-          {tracks.map((track) => (
-            <li key={track.id} className={`relative rounded-lg shadow-md p-2 flex flex-col items-center ${imageLoaded ? 'animate__animated animate__fadeInUp' : 'hidden'}`} style={{ backgroundColor: sampleColors[track.id] }}>
-              <div className="relative w-full" id="spotify-tracks-rest">
-                <img
-                  src={track.album.images[0]?.url}
-                  alt={track.name}
-                  onLoad={() => {
-                    setTimeout(() => setImageLoaded(true), 1000); // Delay the animation start
-                  }}
-                  className="w-full object-contain shadow-xl rounded-md mb-2"
-                />
-                <div className="absolute inset-0 bg-black bg-opacity-40 opacity-0 transition-opacity flex flex-col items-center justify-center p-4 w-full" id="spotify-tracks-rest-details">
-                  <p className="text-sm text-gray-300 text-center mb-2">Album: {track.album.name}</p>
-                  <p className="text-sm text-gray-300 text-center mb-2">Track: {track.track_number} of {track.album.total_tracks}</p>
-                  <p className="text-sm text-gray-300 text-center mb-2">By: {track.artists.map(artist => artist.name).join(', ')}</p>
-                </div>
-                <p className="text-lg font-semibold text-center spotify-track-title">{removeTextInParentheses(track.name)}</p>
-              </div>
-              <button onClick={() => handleSelect(track.id, 'track')} className="mt-2 px-4 py-2 bg-blue-500 text-white rounded">Select Track</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <h1>Choose Artist</h1>
-      <div className="max-w-screen-lg mx-auto p-4 h-auto w-full text-white flex flex-col items-center justify-center">
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 w-full">
-          {artists.map((artist) => (
-            <li key={artist.id} className={`min-h-fit relative rounded-lg shadow-md p-2 flex flex-col items-center justify-center ${imageLoaded ? 'animate__animated animate__fadeInUp' : 'hidden'}`} style={{ backgroundColor: sampleColors[artist.id] }}>
-              <div className="w-full">
-                <img
-                  src={artist.images[0]?.url}
-                  alt={artist.name}
-                  className="shadow-xl rounded-md mb-2"
-                  onLoad={() => {
-                    setTimeout(() => setImageLoaded(true), 1000); // Delay the animation start
-                  }}
-                />
-                <div className="absolute inset-0 h-fit bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center p-4">
-                  <p className="text-sm text-gray-300 text-center mb-2">Popularity: {artist.popularity}</p>
-                  <p className="text-sm text-gray-300 text-center mb-2">Followers: {artist.followers.total}</p>
-                  <p className="text-sm text-gray-300 text-center mb-2">{artist.genres.map(genres => genres).join(', ')}</p>
-                </div>
-                <div>
-                  <button onClick={() => handleSelect(artist.id, 'artist')} className="mt-2 px-4 py-2 bg-green-500 text-white rounded">Select Artist</button>
-                  <p className="text-lg font-semibold text-center spotify-artist-title object-contain text-nowrap">{removeTextInParentheses(artist.name)}</p>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <h1>Choose Genre</h1>
-    </div>
+    <div className="flex flex-col items-center">
+  <h1 className="font-sans text-4xl md:text-5xl lg:text-6xl text-white text-center leading-tight tracking-tight font-bold my-4 mx-auto max-w-2xl" id="top-track-heading">
+    Choose Tracks
+  </h1>
+  <div className="max-w-screen-lg h-full w-fit my-8 text-white">
+    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
+      {tracks.map((track) => (
+        <li
+          key={track.id}
+          className={`relative rounded-lg shadow-md p-4 flex flex-col items-center bg-gray-700 ${imageLoaded ? 'animate__animated animate__fadeInUp' : 'hidden'}`}
+          style={{ backgroundColor: sampleColors[track.id] }}
+        >
+          <div className="relative w-full" id="spotify-tracks-rest">
+            <img
+              src={track.album.images[0]?.url}
+              alt={track.name}
+              onLoad={() => {
+                setTimeout(() => setImageLoaded(true), 1000); // Delay the animation start
+              }}
+              className="w-full object-contain shadow-xl rounded-md mb-2"
+            />
+            <div className="absolute inset-0 bg-black bg-opacity-40 opacity-0 transition-opacity flex flex-col items-center justify-center p-4 w-full" id="spotify-tracks-rest-details">
+              <p className="text-sm text-gray-300 text-center mb-2">Album: {track.album.name}</p>
+              <p className="text-sm text-gray-300 text-center mb-2">Track: {track.track_number} of {track.album.total_tracks}</p>
+              <p className="text-sm text-gray-300 text-center mb-2">By: {track.artists.map(artist => artist.name).join(', ')}</p>
+            </div>
+            <p className="text-lg font-semibold text-center spotify-track-title">{removeTextInParentheses(track.name)}</p>
+          </div>
+          <button onClick={() => handleSelect(track.id, 'track')} className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
+            Select Track
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+
+  <h1 className="font-sans text-4xl md:text-5xl lg:text-6xl text-white text-center leading-tight tracking-tight font-bold my-8 mx-auto max-w-2xl" id="top-track-heading">
+    Choose Artist
+  </h1>
+  <div className="max-w-screen-lg mx-auto p-4 h-auto w-full my-8 text-white flex flex-col items-center justify-center">
+    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
+      {artists.map((artist) => (
+        <li
+          key={artist.id}
+          className={`min-h-fit relative rounded-lg shadow-md p-4 flex flex-col bg-green-900 items-center justify-center ${imageLoaded ? 'animate__animated animate__fadeInUp' : 'hidden'}`}
+          style={{ backgroundColor: sampleColors[artist.id] }}
+        >
+          <div className="w-fit">
+            <img
+              src={artist.images[0]?.url}
+              alt={artist.name}
+              className="shadow-xl rounded-md mb-2 w-full"
+              onLoad={() => {
+                setTimeout(() => setImageLoaded(true), 1000); // Delay the animation start
+              }}
+            />
+            <div className="absolute inset-0 h-fit bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center p-4">
+              <p className="text-sm text-gray-300 text-center mb-2">Popularity: {artist.popularity}</p>
+              <p className="text-sm text-gray-300 text-center mb-2">Followers: {artist.followers.total}</p>
+              <p className="text-sm text-gray-300 text-center mb-2">{artist.genres.map(genre => genre).join(', ')}</p>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-lg font-semibold text-center spotify-artist-title">{removeTextInParentheses(artist.name)}</p>
+              <button onClick={() => handleSelect(artist.id, 'artist')} className="mt-2 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition">
+                Select Artist
+              </button>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  </div>
+
+  <h1 className="font-sans text-4xl md:text-5xl lg:text-6xl text-white text-center leading-tight tracking-tight font-bold my-4 mx-auto max-w-2xl" id="top-track-heading">
+    Choose Genre
+  </h1>
+</div>
   );
 }
