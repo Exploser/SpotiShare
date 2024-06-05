cbd2c67fe8aff9957e9df12c3a53cf75e1fdc62b
Exploser <exploserbusiness@gmail.com>
2024-06-04 20:52:15 -0400

    Add types to get genres

--- src/pages/api/spotifyAPICalls/getGenres.ts
+++ src/pages/api/spotifyAPICalls/getGenres.ts
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         if (!accessToken) throw new Error('Spotify access token not found');
 
         
-        const getGenres = await fetchSpotifyGenres(accessToken);
+        const getGenres: Array<string> = await fetchSpotifyGenres(accessToken);
 
         res.status(200).json(getGenres);
     } catch (error) {
