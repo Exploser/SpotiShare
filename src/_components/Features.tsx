8756f04c54f429f6c56f2bdc544dd89fa35d8b9b
Exploser <exploserbusiness@gmail.com>
2024-06-13 21:08:12 -0400

    Update Features

--- src/_components/Features.tsx
+++ src/_components/Features.tsx
@@ -1,8 +1,7 @@
 const SpotifyFeatures: React.FC = () => {
     return (
-        <div className="flex flex-col items-center bg-opacity-10 p-6 rounded-xl my-8"
+        <div className="flex flex-col items-center bg-opacity-10 rounded-xl my-8"
             id="spotify-features">
-            <h1 className="text-3xl font-bold text-white mb-4">Features!</h1>
             <div id="spotify-features-buttons">
                 <a href="/toptracks" className="flex flex-col justify-center items-center bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 border border-green-700 rounded whitespace-nowrap text-center my-4 h-fit hover:scale-105">
                     <div>
