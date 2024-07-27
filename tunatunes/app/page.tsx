50b65010022b27df1a0ed94603d4f200afe1ee52
Exploser <exploserbusiness@gmail.com>
2024-07-27 15:45:58 -0400

    Implement landing page

--- tunatunes/app/page.tsx
+++ tunatunes/app/page.tsx
@@ -1,10 +1,15 @@
+'use client';
+import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
-import Image from "next/image";
+import SpotifyProfile from "@/components/spotify-profile";
 
 export default function Home() {
+
   return (
-    <div className="h-full">
+    <div className="h-full bg-[#111827]">
       <LandingNavbar />
+      <LandingHero />
+      <SpotifyProfile />
     </div>
   );
 }
