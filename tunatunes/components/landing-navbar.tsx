f32ea2ca21757e17c4c0440a70ccfa15a95d2ecc
Exploser <exploserbusiness@gmail.com>
2024-07-27 15:47:02 -0400

    Implement landing-navbar

--- tunatunes/components/landing-navbar.tsx
+++ tunatunes/components/landing-navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { useAuth } from "@clerk/nextjs";
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,7 +9,6 @@ import { Button } from "@/components/ui/button";
 const font = Montserrat({ subsets: ["latin"], weight: '600' });
 
 export const LandingNavbar = () => {
-    const { isSignedIn } = useAuth();
 
     return (
         <nav className=" p-4 bg-transparent flex items-center justify-between">
@@ -18,10 +16,10 @@ export const LandingNavbar = () => {
                 <div className="relative w-8 h-8 mr-4">
                     <Image src="/logo.svg" fill alt="logo" />
                 </div>
-                <h1 className={cn("text-2xl font-bold text-white", font.className)}>Loser AI</h1>
+                <h1 className={cn("text-2xl font-bold text-white", font.className)}>TunaTunes</h1>
             </Link>
             <div className="flex items-center gap-x-2">
-                <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+                <Link href={"/sign-up"}>
                 <Button variant={"outline"} className="rounded-full">
                     Get Started
                 </Button>
