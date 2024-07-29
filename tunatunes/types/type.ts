d90ebf16d8353ec848ad68f7e97111fd87513498
Exploser <exploserbusiness@gmail.com>
2024-07-29 13:33:52 -0400

    Implement types for Spotify Top Tracks Res

--- tunatunes/types/type.ts
+++ tunatunes/types/type.ts
@@ -1,47 +1,70 @@
 export interface SpotifyTrack {
-    id: string;
+  id: string;
+  name: string;
+  artists: Array<{ name: string }>;
+  album: {
     name: string;
-    artists: Array<{ name: string }>;
-    album: {
-      name: string;
-      images: Array<{ url: string }>;
-    };
-    external_urls: {
-      spotify: string;
-    };
-  }
-  export interface SpotifyError {
-    error: {
-      status: number;
-      message: string;
-    };
-  }
-  export interface SpotifyUser {
-    display_name: string;
-    email: string;
-    id: string;
-    images: Array<{ url: string; height: number; width: number }>;
-    followers: { total: number };
-    external_urls: { spotify: string };
-    href: string;
-    type: string;
-    uri: string;
-  }
-  export interface SpotifyTopTracksResponse {
-    items: SpotifyTrack[];
-  }
-  export interface SpotifyTopArtistsResponse {
-    items: Artist[];
-  }
-  export interface Artist {
-    name: string;
-    id: string;
-    external_urls: {
-        spotify: string;
-    };
+    images: Array<{ url: string }>;
+  };
+  external_urls: {
     spotify: string;
-    images: Array<{ url: string; height: number; width: number }>;
-    followers: { total: number };
-    genres: string[];
-    popularity: number;
+  };
+}
+export interface SpotifyError {
+  error: {
+    status: number;
+    message: string;
+  };
+}
+export interface SpotifyUser {
+  display_name: string;
+  email: string;
+  id: string;
+  images: Array<{ url: string; height: number; width: number }>;
+  followers: { total: number };
+  external_urls: { spotify: string };
+  href: string;
+  type: string;
+  uri: string;
+}
+
+export interface SpotifyTopArtistsResponse {
+  items: Artist[];
+}
+
+export interface Artist {
+  name: string;
+  id: string;
+  external_urls: {
+    spotify: string;
+  };
+  spotify: string;
+  images: Array<{ url: string; height: number; width: number }>;
+  followers: { total: number };
+  genres: string[];
+  popularity: number;
+}
+export interface Album {
+  id: string;
+  name: string;
+  release_date: string;
+  total_tracks: number;
+  images: Array<{ url: string; height: number; width: number }>;
+}
+
+export interface Tracks {
+  id: string;
+  name: string;
+  artists: Artist[];
+  album: Album;
+  preview_url: string;
+  track_number: number;
+  popularity: number;
+  external_urls: {
+    spotify: string;
+  };
+}
+
+export interface SpotifyTopTracksResponse {
+  items: Tracks[];
 }
