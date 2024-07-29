be6a44944131539718208fad06320254521d0763
Exploser <exploserbusiness@gmail.com>
2024-07-29 13:33:12 -0400

    Add Top Tracks to dashboard

--- tunatunes/app/(routes)/dashboard/page.tsx
+++ tunatunes/app/(routes)/dashboard/page.tsx
@@ -1,18 +1,18 @@
 'use client';
 import SpotifyProfile from "@/components/spotify-profile";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
-import { Artist, SpotifyTopArtistsResponse } from "@/types/type";
-import Link from "next/link";
+import { Artist, SpotifyTopArtistsResponse, SpotifyTopTracksResponse, Tracks } from "@/types/type";
 import { useEffect, useState } from "react";
 import TopArtistsCarousel from "./components/top-artists-carousel";
+import TopTracksCarousel from "./components/top-tracks-carousel";
 
 const DashboardPage = () => {
     const [artists, setArtitst] = useState<Artist[]>([]);
+    const [tracks, setTracks] = useState<Tracks[]>([]);
     const [error, setError] = useState<string | null>(null);
     const [timeRange, setTimeRange] = useState('medium_term');
     const [limit, setLimit] = useState(19);
 
-    const buildSpotifyAPIUrl = (timeRange = 'medium_term', limit = 19, offset = 0) => {
+    const buildSpotifyArtistsURL = (timeRange = 'medium_term', limit = 19, offset = 0) => {
         const baseUrl = '/api/spotify/getTopArtists';
         const params = new URLSearchParams({
             time_range: timeRange,
@@ -21,9 +21,18 @@ const DashboardPage = () => {
         });
         return `${baseUrl}?${params.toString()}`;
     };
-    const fetchTopTracks = async (timeRange: string, limit: number) => {
+    const buildSpotifyTracksURL = (timeRange = 'medium_term', limit = 19, offset = 0) => {
+        const baseUrl = '/api/spotify/getTopTracks';
+        const params = new URLSearchParams({
+            time_range: timeRange,
+            limit: limit.toString(),
+            offset: offset.toString(),
+        });
+        return `${baseUrl}?${params.toString()}`;
+    };
+    const fetchTopArtists = async (timeRange: string, limit: number) => {
         try {
-            const url = buildSpotifyAPIUrl(timeRange, limit);
+            const url = buildSpotifyArtistsURL(timeRange, limit);
             const response = await fetch(url);
             if (!response.ok) {
                 throw new Error('Failed to fetch top artists');
@@ -38,8 +47,27 @@ const DashboardPage = () => {
         console.log(error);
         return error;
     };
+    const fetchTopTracks = async (timeRange: string, limit: number) => {
+        try {
+            const url = buildSpotifyTracksURL(timeRange, limit);
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Failed to fetch top tracks');
+            }
+            const data: SpotifyTopTracksResponse = await response.json() as SpotifyTopTracksResponse;
+            setTracks(data.items);
+            console.log(data.items);
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message);
+            }
+        }
+        console.log(error);
+        return error;
+    };
 
     useEffect(() => {
+        fetchTopArtists(timeRange, limit).catch((err) => console.error(err));
         fetchTopTracks(timeRange, limit).catch((err) => console.error(err));
     }, []);
     return (
@@ -49,9 +77,15 @@ const DashboardPage = () => {
             <div className="flex justify-center items-center w-full">
                 <SpotifyProfile />
             </div>
+
             <h3 className="font-bold text-2xl py-8 px-4"> Top Artists</h3>
             <div className="w-fit">
                 <TopArtistsCarousel artists={artists} />
+            </div>
+
+            <h3 className="font-bold text-2xl py-8 px-4"> Top Tracks</h3>
+            <div className="w-fit">
+                <TopTracksCarousel tracks={tracks} />
             </div>
         </div>
     );
