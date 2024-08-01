3fc2b19f9dc7c9edc104dbc5b255610e09a3799c
Exploser <exploserbusiness@gmail.com>
2024-07-31 23:07:20 -0400

    Update the carousel component with autoplay plugin

--- tunatunes/app/(routes)/dashboard/components/top-tracks-carousel.tsx
+++ tunatunes/app/(routes)/dashboard/components/top-tracks-carousel.tsx
@@ -1,5 +1,8 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { Tracks } from "@/types/type";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
 import Link from "next/link";
 
 interface TopArtistsCarouselProps {
@@ -8,7 +11,15 @@ interface TopArtistsCarouselProps {
 
 const TopArtistsCarousel = ({ tracks }: TopArtistsCarouselProps) => {
     return (
-        <Carousel className="w-[60vw] ml-20">
+        <Carousel
+            plugins={[
+                Autoplay({
+                    delay: 5000,
+                    stopOnMouseEnter: true,
+                    stopOnLastSnap: true,
+                }),
+            ]}
+            className="w-[60vw] ml-20">
             <CarouselContent>
                 {tracks.slice(0, 5).map((track) => (
                     <CarouselItem key={track.id} className="pl-1 md:basis-1/2 lg:basis-1/3">
