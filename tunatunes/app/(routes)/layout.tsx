69de70ccbc3e70aca696d7762030a469131e4883
Exploser <exploserbusiness@gmail.com>
2024-07-31 23:09:42 -0400

    Fix css bug

--- tunatunes/app/(routes)/layout.tsx
+++ tunatunes/app/(routes)/layout.tsx
@@ -6,7 +6,7 @@ const DashboardLayout = async ({
 }: {children :ReactNode}) => {
 
     return (  
-        <div className="dashboard-layout__content h-full relative bg-[#ffffff]">
+        <div className="dashboard-layout__content relative bg-[#ffffff] min-h-full">
             <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[50] bg-slate-900 md:w-72">
                 <div>
                     <Sidebar />
