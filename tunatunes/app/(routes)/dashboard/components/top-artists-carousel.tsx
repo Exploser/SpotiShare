5ae74e786e0b41bcd03082b7bd5717ffd88f492e
Exploser <exploserbusiness@gmail.com>
2024-07-31 23:29:32 -0400

    Update Icon to redirect to /artists

--- tunatunes/app/(routes)/dashboard/components/top-artists-carousel.tsx
+++ tunatunes/app/(routes)/dashboard/components/top-artists-carousel.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Autoplay from "embla-carousel-autoplay"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
+import { Music2Icon} from "lucide-react";
 
 interface TopArtistsCarouselProps {
     artists: Artist[];
@@ -24,7 +25,7 @@ const TopArtistsCarousel = ({ artists }: TopArtistsCarouselProps) => {
                 {artists.slice(0, 5).map((artist) => (
                     <CarouselItem key={artist.id} className="pl-1 md:basis-1/2 lg:basis-1/3">
                         <div className="flex flex-col items-center justify-center p-2">
-                            <Card className="xs:min-h-full xs:min-w-full bg-center bg-cover bg-no-repeat" style={{backgroundImage: `url(${artist.images[0]?.url})`}}>
+                            <Card className="xs:min-h-full xs:min-w-full bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${artist.images[0]?.url})` }}>
                                 <CardHeader className="bg-black bg-opacity-60 rounded-xl items-center justify-center">
                                     <Image src={artist.images[0]?.url} alt={artist.name} width={200} height={200} />
                                     <CardTitle className="text-white text-center">{artist.name}</CardTitle>
@@ -34,10 +35,15 @@ const TopArtistsCarousel = ({ artists }: TopArtistsCarouselProps) => {
                     </CarouselItem>
                 ))}
                 <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
-                    <div className="flex flex-col items-center justify-center p-2">
-                        <Link href="/artists">
-                            <p className="text-blue-500 hover:underline">More Artists</p>
-                        </Link>
+                    <div className="flex flex-col items-center justify-center p-2 h-full">
+                        <Card className="xs:min-h-full xs:min-w-full bg-slate-500 ">
+                            <CardHeader className="rounded-xl items-center justify-center">
+                                <Music2Icon height={200} width={200} />
+                                <Link href="/artists">
+                                    <CardTitle className="text-white text-center">More Artists</CardTitle>
+                                </Link>
+                            </CardHeader>
+                        </Card>
                     </div>
                 </CarouselItem>
             </CarouselContent>
