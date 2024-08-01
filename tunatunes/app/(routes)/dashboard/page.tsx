a31861ce7726de55218b2e46ac82d05d30a91187
Exploser <exploserbusiness@gmail.com>
2024-07-31 23:09:02 -0400

    Update dashboard fetch functions to fetch short-term data

--- tunatunes/app/(routes)/dashboard/page.tsx
+++ tunatunes/app/(routes)/dashboard/page.tsx
@@ -9,10 +9,10 @@ const DashboardPage = () => {
     const [artists, setArtitst] = useState<Artist[]>([]);
     const [tracks, setTracks] = useState<Tracks[]>([]);
     const [error, setError] = useState<string | null>(null);
-    const [timeRange, setTimeRange] = useState('medium_term');
-    const [limit, setLimit] = useState(19);
+    const timeRange = 'short_term';
+    const limit = 5;
 
-    const buildSpotifyArtistsURL = (timeRange = 'medium_term', limit = 19, offset = 0) => {
+    const buildSpotifyArtistsURL = (timeRange = 'short_term', limit = 5, offset = 0) => {
         const baseUrl = '/api/spotify/getTopArtists';
         const params = new URLSearchParams({
             time_range: timeRange,
@@ -21,7 +21,7 @@ const DashboardPage = () => {
         });
         return `${baseUrl}?${params.toString()}`;
     };
-    const buildSpotifyTracksURL = (timeRange = 'medium_term', limit = 19, offset = 0) => {
+    const buildSpotifyTracksURL = (timeRange = 'short_term', limit = 5, offset = 0) => {
         const baseUrl = '/api/spotify/getTopTracks';
         const params = new URLSearchParams({
             time_range: timeRange,
@@ -71,13 +71,12 @@ const DashboardPage = () => {
         fetchTopTracks(timeRange, limit).catch((err) => console.error(err));
     }, []);
     return (
-        <div className="">
+        <div className="min-h-full">
             <h1 className="font-semibold text-4xl p-4">Dashboard</h1>
             <h3 className="font-bold text-2xl py-8 px-4"> Hello, </h3>
             <div className="flex justify-center items-center w-full">
                 <SpotifyProfile />
             </div>
-
             <h3 className="font-bold text-2xl py-8 px-4"> Top Artists</h3>
             <div className="w-fit">
                 <TopArtistsCarousel artists={artists} />
